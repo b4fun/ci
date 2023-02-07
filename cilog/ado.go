057b4f662b72fd3c5b0c7eda709d8d438665ccd7
hbc <bcxxxxxx@gmail.com>
2023-02-07 21:11:38 +0800

    feat: implement ado logger

---
+++ cilog/ado.go
@@ -0,0 +1,81 @@
+package cilog
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// AzurePipelineOpts configures AzurePipeline logger.
+// refs:
+// - https://learn.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands
+type AzurePipelineOpts interface {
+	apply(*azurePipelineT)
+}
+
+type azurePipelineOptsFunc func(apt *azurePipelineT)
+
+func (f azurePipelineOptsFunc) apply(apt *azurePipelineT) {
+	f(apt)
+}
+
+// azurePipelineT implements Logger for AzurePipelines CI.
+type azurePipelineT struct {
+	Mute
+
+	out io.Writer // reserve for testing only for now
+}
+
+var _ Logger = (*azurePipelineT)(nil)
+
+// AzurePipeline creates an AzurePipeline logger.
+func AzurePipeline(opts ...AzurePipelineOpts) Logger {
+	rv := &azurePipelineT{
+		out: os.Stdout,
+	}
+
+	for _, o := range opts {
+		o.apply(rv)
+	}
+
+	return rv
+}
+
+func (apt *azurePipelineT) Capabilities() map[Capability]struct{} {
+	return map[Capability]struct{}{
+		CapabilityLog:        {},
+		CapabilityDebugLog:   {},
+		CapabilityWarningLog: {},
+		CapabilityErrorLog:   {},
+		CapabilityGroupLog:   {},
+	}
+}
+
+func (apt *azurePipelineT) logfln(s string, a ...interface{}) {
+	fmt.Fprintf(apt.out, s, a...)
+	fmt.Fprintln(apt.out)
+}
+
+func (apt *azurePipelineT) Log(s string) {
+	apt.logfln(s)
+}
+
+func (apt *azurePipelineT) DebugLog(s string) {
+	apt.logfln("##[debug]%s", s)
+}
+
+func (apt *azurePipelineT) WarningLog(s string) {
+	apt.logfln("##[warning]%s", s)
+}
+
+func (apt *azurePipelineT) ErrorLog(s string) {
+	apt.logfln("##[error]%s", s)
+}
+
+func (apt *azurePipelineT) GroupLog(params GroupLogParams) (Logger, func()) {
+	apt.logfln("##[group]%s", params.Name)
+	closeSection := func() {
+		apt.logfln("##[endgroup]")
+	}
+	return apt, closeSection
+}
---
+++ cilog/ado_test.go
@@ -0,0 +1,58 @@
+package cilog
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/b4fun/ci"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAzurePipeline(t *testing.T) {
+	b := new(bytes.Buffer)
+	apt := AzurePipeline(azurePipelineOptsFunc(func(apt *azurePipelineT) {
+		apt.out = b
+	}))
+
+	caps := apt.Capabilities()
+	assert.Contains(t, caps, CapabilityLog)
+	assert.Contains(t, caps, CapabilityDebugLog)
+	assert.Contains(t, caps, CapabilityWarningLog)
+	assert.Contains(t, caps, CapabilityErrorLog)
+	assert.Contains(t, caps, CapabilityGroupLog)
+
+	apt.Log("foobar")
+	apt.DebugLog("debug foobar")
+	apt.WarningLog("warning foobar")
+	apt.ErrorLog("error foobar")
+	func() {
+		gapt, closeGroup := apt.GroupLog(GroupLogParams{Name: "group"})
+		defer closeGroup()
+
+		gapt.Log("foobar")
+		gapt.DebugLog("debug foobar")
+		gapt.WarningLog("warning foobar")
+		gapt.ErrorLog("error foobar")
+	}()
+
+	output := b.String()
+	if ci.Detect() != ci.AzurePipelines {
+		t.Log(output)
+	}
+
+	assert.Equal(
+		t,
+		`foobar
+##[debug]debug foobar
+##[warning]warning foobar
+##[error]error foobar
+##[group]group
+foobar
+##[debug]debug foobar
+##[warning]warning foobar
+##[error]error foobar
+##[endgroup]
+`,
+		output,
+	)
+}
